821e126328405518958cdf6f8ba59e9dbef30a16
OutisMed <medardkik01@gmail.com>
2025-01-27 15:43:00 +0100

    Correction erreur

--- Affirmation/app/src/main/java/com/example/affirmation/model/Affirmation.kt
+++ Affirmation/app/src/main/java/com/example/affirmation/model/Affirmation.kt
@@ -2,9 +2,8 @@ package com.example.affirmation.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-class Affirmation {
+
     data class Affirmation(
         @StringRes val stringResourceId: Int,
         @DrawableRes val imageResourceId: Int
     )
-}
